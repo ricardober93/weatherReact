cb56f74543ee87a7ef55f72074982cb52ebb915f
ricardober93 <39892056+ricardober93@users.noreply.github.com>
2018-11-29 11:35:25 -0500

    Add files via upload

---
+++ precache-manifest.ee6d2a3175469675945e02662808e1e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da661c60d23d29cf0b6e",
+    "url": "/static/css/main.5e8d5d5d.chunk.css"
+  },
+  {
+    "revision": "da661c60d23d29cf0b6e",
+    "url": "/static/js/main.da661c60.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "408bca62617e82a7c6c2f3714a549211",
+    "url": "/index.html"
+  }
+];
